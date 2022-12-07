670b2ec7938c11f669ca44e51f83a566fccb8c54
evgenijbelov <beles123@bk.ru>
2022-12-07 15:38:40 +0300

    все кроме доп2

---
+++ zdop1/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 32);
+        
+}
+
+
+void ReleaseArray(int[] array)
+{
+    int count3 = 0, count4 = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+         if (array[i] %2 !=0)
+        {
+            Console.Write(array[i] + "  ");
+            count3 ++;
+        }
+        }
+        Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] %2 ==0)
+        {
+            Console.Write(array[i] + "  ");
+            count4 ++;
+        }
+        }
+    Console.WriteLine();
+    
+    if (count4>count3)
+    Console.WriteLine("yes");
+    else
+    Console.WriteLine("no"); 
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива от 1 до 100: ");
+int n = int.Parse(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
---
+++ zdop2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Входной файл INPUT.TXT содержит чётное число N (4 ≤ N ≤ 998).
+//В выходной файл OUTPUT.TXT необходимо вывести два простых числа, сумма которых равна числу N. 
+//Первым выводится наименьшее число.
+
+Console.Clear();
+Console.WriteLine("введите четное число");
+int a=2;
+int b=2;
+int n = int.Parse(Console.ReadLine());
+if (n%2!=0 || n<=4 || n>998)
+Console.Write("введите четное число");
+else
+{
+      a = new Random().Next(2,n);
+while (a<=n/2 && a%1==0 && a%a==0)
+{
+   a = new Random().Next(2,n);
+   b=n-a;
+}
+}
+    
+    
+    
+Console.WriteLine(a);
+Console.WriteLine(b);
+
